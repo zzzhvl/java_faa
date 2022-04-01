7cb303a91ac2520da92c90946fab0cd12b9c999a
zzzhvl <vladimirzharov11@gmail.com>
2022-04-02 00:31:03 +0300

    Add some functions

--- sandbox/src/main/java/ru/zzzhvl/faa/sandbox/DooBee.java
+++ sandbox/src/main/java/ru/zzzhvl/faa/sandbox/DooBee.java
@@ -2,14 +2,27 @@ package ru.zzzhvl.faa.sandbox;
 
 public class DooBee {
     public static void main(String[] args) {
-        int x = 1;
-        while (x < 3) {
-            System.out.print("Doo");
-            System.out.print("Bee");
-            x = x + 1;
-        }
-        if (x == 3) {
-            System.out.print("Do");
-        }
+        hello("world");
+        hello("Vova");
+        hello("user");
+
+        double l = 5;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+        double a = 4;
+        double b =6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
     }
 }
