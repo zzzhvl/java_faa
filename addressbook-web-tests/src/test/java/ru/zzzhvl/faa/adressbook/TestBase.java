148708b85d941542ec620e0938b02be6294a0232
zzzhvl <vladimirzharov11@gmail.com>
2022-04-05 22:38:01 +0300

    Optimize TestBase class

--- addressbook-web-tests/src/test/java/ru/zzzhvl/faa/adressbook/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/zzzhvl/faa/adressbook/TestBase.java
@@ -69,39 +69,6 @@ public class TestBase {
         }
     }
 
-    private boolean isElementPresent(By by) {
-        try {
-            driver.findElement(by);
-            return true;
-        } catch (NoSuchElementException e) {
-            return false;
-        }
-    }
-
-    private boolean isAlertPresent() {
-        try {
-            driver.switchTo().alert();
-            return true;
-        } catch (NoAlertPresentException e) {
-            return false;
-        }
-    }
-
-    private String closeAlertAndGetItsText() {
-        try {
-            Alert alert = driver.switchTo().alert();
-            String alertText = alert.getText();
-            if (acceptNextAlert) {
-                alert.accept();
-            } else {
-                alert.dismiss();
-            }
-            return alertText;
-        } finally {
-            acceptNextAlert = true;
-        }
-    }
-
     protected void deleteSelectedGroups() {
         driver.findElement(By.name("delete")).click();
     }
