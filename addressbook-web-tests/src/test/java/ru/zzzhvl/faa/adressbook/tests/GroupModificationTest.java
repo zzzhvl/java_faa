8258034989dbb2ae2f6b7fce7fbd0e837e2854ec
zzzhvl <vladimirzharov11@gmail.com>
2022-04-06 20:05:33 +0300

    Add new test "GroupModificationTest"

---
+++ addressbook-web-tests/src/test/java/ru/zzzhvl/faa/adressbook/tests/GroupModificationTest.java
@@ -0,0 +1,17 @@
+package ru.zzzhvl.faa.adressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.zzzhvl.faa.adressbook.model.GroupData;
+
+public class GroupModificationTest extends TestBase {
+
+    @Test
+    public void testGroupModification(){
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returntoGroupPage();
+    }
+}
